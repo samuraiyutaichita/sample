fe97103ca97d237e063050960543156fe7b3777f
EC2 Default User <ec2-user@ip-172-31-26-230.ap-northeast-1.compute.internal>
2021-12-21 04:56:51 +0000

    first commit

---
+++ test.php
@@ -0,0 +1,16 @@
+<head>
+    <meta charset="utf-8">
+</head>
+<h1>Hello World!</h1>
+<h2>Let's start HTML and CSS!</h2>
+<ul>
+<?php
+
+for ($i = 0; $i < 5; $i++) {
+
+    echo "<li>"."あかさたな"."</li>";
+
+}
+?>
+
+</ul>
